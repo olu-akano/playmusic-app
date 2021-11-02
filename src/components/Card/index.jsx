0b43f528f7afa761a2dfc79e5adcf241a72ba592
olu-akano <akanopaul@yahoo.co.uk>
2021-11-02 22:07:54 +0000

    add hover functionality

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../Card/style.css';
 import {LikeButton} from '../LikeButton/index'
 
 export const Card = ({ size, name, genre, intro }) => {
+    const [showIntro, setshowIntro] =useState();
+
+    const mouseEnter = () => setshowIntro(true);
+    const mouseLeave = () => setshowIntro(false);
+
     return (
         <>
-            <div className={`card ${size}`}>
+            <div className={`card ${size}`}
+            onMouseEnter={mouseEnter}
+            onMouseLeave={mouseLeave}>
                 <h3>{name}</h3>
                 <em>{genre}</em>
-                <h4>{intro}</h4>
+                <h4>{showIntro ? intro: "Hover for more..."}</h4>
                 <LikeButton/>
-        </div>
+            </div>
         </>
     )
 }
