39b80d89580dcbb7c75bd771e975b8b90f19f2be
olu-akano <akanopaul@yahoo.co.uk>
2021-11-02 17:01:37 +0000

    add production webpack configuration

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
-    path: path.join(__dirname, '../dist'),
+    path: path.join(__dirname, '/build'),
     filename: 'index.bundle.js',
     publicPath: '/'
   },
@@ -14,7 +14,7 @@ module.exports = {
         resolve: {
           extensions: [".js", ".jsx"]
         },
-        exclude: /nodeModules/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
