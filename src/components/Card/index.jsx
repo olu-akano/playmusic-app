f5040ecb36314a9be93327b6d7f4d0060ab0a10e
olu-akano <akanopaul@yahoo.co.uk>
2021-11-02 15:02:23 +0000

    add like buton and test it

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../Card/style.css';
+import {LikeButton} from '../LikeButton/index'
 
 export const Card = ({ size, name, genre, intro }) => {
     return (
+        <>
         <div className={`card ${size}`}>
             <h3>{name}</h3>
             <em>{genre}</em>
             <h4>{intro}</h4>
+            <LikeButton/>
         </div>
+        </>
     )
 }
