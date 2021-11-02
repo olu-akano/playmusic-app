21a9f5a28fa1ffcd2d8c1d30c9a6fa1a9ed037b9
olu-akano <akanopaul@yahoo.co.uk>
2021-11-02 17:53:28 +0000

    remove dist and build folders

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, '../dist'),
     filename: 'index.bundle.js',
     publicPath: '/'
   },
