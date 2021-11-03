38aed1409e44851bf367c6a87fa0106f99b70e86
“yourGithubUsername” <your@emailadd.com>
2021-11-03 15:50:18 +0000

    navigation bar and style

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -5,9 +5,9 @@ import './style.css'
 export const Header = () => {
     return(
         <nav>
-            <Link ClassName='Navbar' exact to='/'>Home</Link>
-            <NavLink ClassName='Navbar' to='https://www.youtube.com/watch?v=eldeaIAv_wE'>Song of the Day</NavLink>
-            <NavLink ClassName='Navbar' to='https://open.spotify.com/playlist/37i9dQZF1DXan38dNVDdl4'>Playlist</NavLink>
+            <Link exact to='/'>Home</Link>
+            <Link to='https://www.youtube.com/watch?v=eldeaIAv_wE'>Song of the Day</Link>
+            <Link to='https://open.spotify.com/playlist/37i9dQZF1DXan38dNVDdl4'>Playlist</Link>
         </nav>
     )
 };
