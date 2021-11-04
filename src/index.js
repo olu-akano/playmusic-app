6f0d636de521b71059acc747257ee1dd056f8ee5
“yourGithubUsername” <your@emailadd.com>
2021-11-04 15:24:02 +0000

    add createStore redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore } from 'redux';
+
+
 import App from './App';
 
 ReactDOM.render(
